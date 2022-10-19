b3a312cb7e9f54c5a3669762eb00de77d724a3ce
Jobin <Jobin@baiyanbing.com>
2022-10-19 22:54:38 +0800

    修改表格样式

--- src/main/java/com/byb/tools/doc/utils/DocTool.java
+++ src/main/java/com/byb/tools/doc/utils/DocTool.java
@@ -1,17 +1,23 @@
 package com.byb.tools.doc.utils;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
 import com.spire.doc.*;
 import com.spire.doc.collections.ParagraphCollection;
+import com.spire.doc.documents.BorderStyle;
 import com.spire.doc.documents.TableRowHeightType;
 import com.spire.doc.fields.TextRange;
+import com.spire.doc.formatting.Border;
 
 import java.awt.*;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -48,12 +54,12 @@ public class DocTool {
         Section section = document.addSection();
 
         // 行数
-        int rowsNum = textList.size() / 2 + 1;
+        int rowsNum = textList.size() / 2;
         // 列数
-        int columnsNum = 4;
+        int columnsNum = 3;
 
         // 列宽系数
-        float[] columnsWidthsRate = {0.05f, 0.3f, 0.3f, 0.35f};
+        float[] columnsWidthsRate = {0.08f, 0.46f, 0.46f};
 
         //添加表格
         Table table = section.addTable(true);
@@ -63,17 +69,18 @@ public class DocTool {
         table.resetCells(rowsNum, columnsNum);
 
         // 绘制表头（空白行）
-        TableRow tableHeader = table.getRows().get(0);
-        for(int i =0; i < columnsNum; i++) {
-            TableCell cell = tableHeader.getCells().get(i);
-            tableHeader.setHeightType(TableRowHeightType.Auto);
-            tableHeader.getRowFormat().setBackColor(Color.white);
-            cell.setCellWidth(table.getWidth() * columnsWidthsRate[i], CellWidthType.Point);
-        }
+        //TableRow tableHeader = table.getRows().get(0);
+        //for(int i =0; i < columnsNum; i++) {
+        //    TableCell cell = tableHeader.getCells().get(i);
+        //    tableHeader.setHeightType(TableRowHeightType.Auto);
+        //    tableHeader.getRowFormat().setBackColor(Color.white);
+        //    drawCell(cell, StrUtil.EMPTY, table.getWidth() * columnsWidthsRate[i]);
+        //}
 
+        int seqNum = 0;
         Iterator<String> it = textList.iterator();
         //添加数据到剩余行
-        for (int r = 1; r < rowsNum; r++) {
+        for (int r = 0; r < rowsNum; r++) {
 
             TableRow dataRow = table.getRows().get(r);
             dataRow.setHeightType(TableRowHeightType.Auto);
@@ -83,20 +90,23 @@ public class DocTool {
                 break;
             }
 
-            for(int i =0; i < columnsNum; i++) {
-                TableCell cell = dataRow.getCells().get(i);
-                if(i == 0) {
-                    drawCell(cell, String.valueOf(r), table.getWidth() * columnsWidthsRate[i]);
-                    continue;
-                }
+            String cell2Value = StrUtil.trimToEmpty(it.next());
+            String cell3Value = StrUtil.trimToEmpty(it.next());
 
-                if(i == 1|| i == 2) {
-                    drawCell(cell, it.next(), table.getWidth() * columnsWidthsRate[i]);
-                }
+            TableCell cell0 = dataRow.getCells().get(0);
+            TableCell cell1 = dataRow.getCells().get(1);
+            TableCell cell2 = dataRow.getCells().get(2);
 
-                if(i == 3) {
-                    drawCell(cell, StrUtil.EMPTY, table.getWidth() * columnsWidthsRate[i]);
-                }
+            drawCell(cell1, cell2Value, table.getWidth() * columnsWidthsRate[1]);
+            drawCell(cell2, cell3Value, table.getWidth() * columnsWidthsRate[2]);
+
+            boolean isTopicRow = StrUtil.startWith(cell2Value, "主题") && StrUtil.startWith(cell3Value, "来源");
+            if(isTopicRow){
+                drawCell(cell0, r == 0 ? DateUtil.format(new Date(), "yyyy") :StrUtil.EMPTY, table.getWidth() * columnsWidthsRate[0]);
+                drawCell(cell0, r == 0 ? DateUtil.format(new Date(), "MMdd") :StrUtil.EMPTY, table.getWidth() * columnsWidthsRate[0]);
+                dataRow.getCells().forEach(cell -> ((TableCell)cell).getCellFormat().setBackColor(new Color(128, 198, 135)));
+            }else {
+                drawCell(cell0, String.valueOf(++seqNum), table.getWidth() * columnsWidthsRate[0]);
             }
         }
 
@@ -109,8 +119,19 @@ public class DocTool {
 
     private static void drawCell(TableCell cell, String text, float width) {
         cell.setCellWidth(width, CellWidthType.Point);
+        List<Border> borders = CollUtil.newArrayList(cell.getCellFormat().getBorders().getLeft(),
+                cell.getCellFormat().getBorders().getTop(),
+                cell.getCellFormat().getBorders().getRight(),
+                cell.getCellFormat().getBorders().getBottom());
+        borders.stream().forEach(border -> {
+            border.setBorderType(BorderStyle.Hairline);
+            border.setLineWidth(1.0F);
+            border.setColor(Color.GRAY);
+        });
+
         TextRange textRange = cell.addParagraph().appendText(text);
         textRange.getCharacterFormat().setFontSize(9f);
+        textRange.getCharacterFormat().setTextColor(new Color(89, 89, 89));
     }
 
     public static String uploadToFileServer(byte[] bytes, String filename) {
